3b81f6fd73e0a059514e3a76945e088f0dd63d43
rhw <qorgh2akfl@naver.com>
2023-07-09 19:18:10 +0900

    9625

---
+++ DP/baekjoon9625.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+dp = [[0,0]]*46
+dp[0] = [1,0]
+dp[1] = [0,1]
+
+for i in range(2,n+1):
+
+    dp[i] = [dp[i - 1][1], dp[i - 1][0] + dp[i - 1][1]]
+
+
+
+print(dp[n][0],dp[n][1])
