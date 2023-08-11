90e9617015b42a7186c6347ead39a614cd560f27
rhw <qorgh2akfl@naver.com>
2023-08-11 16:40:31 +0900

    16401

--- BINARYSEARCH/baekjoon16401.py
+++ BINARYSEARCH/baekjoon16401.py
@@ -1,31 +1,18 @@
 import sys
-
-# 조카의 수, 과자의 수
-m, n = map(int, sys.stdin.readline().split())
-# 과자의 길이
-arr = list(map(int, sys.stdin.readline().split()))
-
-start = 0
-end = max(arr)
-
-result = 0
-while (start <= end):
-    total = 0
-    mid = (start + end) // 2
-    # 모든 조카에게 같은 길이의 막대과자를 나눠줄 수 없을 때
-    if mid == 0:
-        total = 0
-        break
-
-    for x in arr:
-        if x >= mid:
-            total += (x // mid)
-
-    if total >= m:
+input = sys.stdin.readline
+m, n  = map(int,input().split())
+L = list(map(int, input().split()))
+start = 1 # 과자의 길이는 1 이상
+end = int(1e9)
+answer = 0
+while start <= end:
+    mid = (start+end)//2
+    c = 0 # 길이가 mid인 것을 만들 수 있는 개수
+    for i in L:
+        c += i//mid
+    if c >=m:
+        answer = max(answer,mid)
         start = mid + 1
-        result = mid
-
     else:
         end = mid - 1
-
-print(result)
+print(answer)
