9292420f43181dc03c0e0c4eb68140c035539004
rhw <qorgh2akfl@naver.com>
2023-08-12 14:41:13 +0900

    17951

---
+++ BINARYSEARCH/baekjoon17951.py
@@ -0,0 +1,25 @@
+N, K = map(int, input().split())
+arr = list(map(int, input().split()))
+
+answer = 0
+left, right = 0, int(1e5)*20+1
+while left <= right:
+    mid = (left+right)//2
+    # 그룹 나누기
+    group = 0
+    score = 0
+    for s in arr:
+
+        score += s
+        if score >= mid:
+            
+            group += 1
+            score = 0
+    # 다음
+    if group >= K:
+        answer = mid
+        left = mid+1
+    else:
+        right = mid-1
+
+print(answer)
