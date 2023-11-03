1944e124c20b2a2c3da209ec18db59a8089df011
howon-ryu <qorgh2akfl@naver.com>
2023-11-03 11:02:24 +0900

    [level 2] Title: 타겟 넘버, Time: 2.82 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,17 +1,13 @@
 def solution(numbers, target):
-
-    def calc(idx, sum):
+    def calc(idx,sum):
         nonlocal answer
-
-        if idx == len(numbers):
-            if sum == target:
-                answer += 1
+        if(idx == len(numbers)):
+            if(sum == target):
+                answer+=1
             return
-
-        calc(idx+1, sum + numbers[idx])
-        calc(idx+1, sum - numbers[idx])
-
+        calc(idx+1,sum+numbers[idx])
+        calc(idx+1,sum-numbers[idx])
     answer = 0
-    calc(0, 0)
-
+    calc(0,0)
     return answer
+    
