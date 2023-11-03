f7bc6dfb881f36564fc78202a63e7f1a70a89b79
rhw <qorgh2akfl@naver.com>
2023-11-04 00:45:51 +0900

    graph

--- practice.py
+++ practice.py
@@ -1,77 +1,20 @@
 from collections import deque
+INF = ie9
 
-def solution(maps):
-    answer = 0
-    n = len(maps)
-    m = len(maps[0])
-
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
-
-    def bfs(x,y):
-        q = deque()
-        q.append((x,y))
-
-        while q:
-            x,y =q.popleft()
-            for i in range(4):
-                nx = x+dx[i]
-                ny = y+dy[i]
-
-                if (nx < 0 or ny < 0 or nx >=n or ny>=m):
-                    continue
-                if(maps[nx][ny]==0):
-                    continue
-                if(maps[nx][ny]==1):
-                    maps[nx][ny] = maps[x][y]+1
-                    q.append((nx,ny))
-        return maps[n-1][m-1]
-
-    if(bfs(0,0)==1):
-        return -1
-    else:
-        answer = bfs(0,0)
-    return answer
-
-
-
-
-
-def solution(nubmers,target):
-    def calc(idx, sum):
-        nonlocal answer
-        if(idx == len(numbers)):
-            if(sum == target):
-                answer+=1
-            return
-        calc(idx+1,sum+numbers[idx])
-        calc(idx + 1, sum - numbers[idx])
-
-    answer = 0
-    calc(0,0)
-    return answer
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def solution(n,edge):
+    graph = {i:[] for i in range(1,n+1)}
+    for i in edge:
+        graph[i[0]].append(i[1])
+        graph[i[1]].append(i[0])
+    q = deque([])
+    
+    distance = [INF]*(n+1)
+    distance[1] = 0
+    q.append(1)
+    while q:
+        now = q.popleft
+        for i in graph[now]:
+            if(distance[i]==INF):
+                distance[i] = distance[now]+1
+                q.append(i)
+    return distance[1:].count(max(distance[1:]))
